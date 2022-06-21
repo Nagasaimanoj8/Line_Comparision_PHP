e3d8e6643664b5d51488ec6c506c21daa85c9a33
unknown <nagasaimanoj333@gmail.com>
2022-06-21 13:37:33 +0530

    [Manoj]Adding_Compare

--- LineComparision/linecomparision.php
+++ LineComparision/linecomparision.php
@@ -31,25 +31,38 @@ class LineComparision
         $value1=pow(($this->x2-$this->x1),2);
        $value2=pow(($this->y2-$this->y1),2);
        $this->length1=sqrt($value1+$value2);
-       echo ("lenght of line1 is" ."\n" .round(($this->length1),2 ."\n"));
+       echo ("lenght of line is" ."\n" .round(($this->length1),2 ."\n"));
        return "lenght of line1 is" ."\n" .round(($this->length1),2 ."\n");
     }   
     //Function to check two line are equals are not
     function CheckEquality(){
         $this->lenght1=$this->calculateLength();
-        $this->length2=$this->calculateLength();
+        $this->lenght2=$this->calculateLength();
+        
         if($this->lenght1 == $this->length2){
             echo "\nBoth lines are euqals";
         }else{
             echo "\nLines are not equals";
         }        
     }
-
-   
+    function compare(){
+       $this->l1=$this->calculateLength();
+       $this->l2=$this->calculateLength();
+       if($this->l1==$this->l2){
+        echo"both the lenghts are equal";
+       }
+       elseif($this->l1>$this->l2){
+        echo "Length 1 is greater than length 2";
+       }
+       else{
+        echo"Lenght 2 is greater than length1";
+       }      
+       
+    }  
 }
 $line = new LineComparision();
 $line->welcome();
 $line->CheckEquality();
-//$line->LineCompare();
+$line->compare();
 
 ?> 
